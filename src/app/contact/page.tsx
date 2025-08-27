776ca6a33ba6f314b2032d73bd6eed1f0f502eba
subinasr <ssr.subina@gmail.com>
2025-08-27 11:57:04 +0545

    feat(map): add map in contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -31,6 +31,8 @@ const contactUsHeading = 'We’d love to hear from you.';
 const contactUsText = 'Have questions, need support or possible partnership? We\'re here to help. Reach out to us through the form below or use the contact details provided — we\'ll get back to you as soon as possible.';
 const reportConcernHeading = 'You can report a concern';
 const reportConcernText = 'If you’ve witnessed or experienced something that concerns you, we encourage you to report it. Your feedback helps us ensure accountability, transparency, and a safer environment for all.';
+const getInTouchHeading = 'Get in touch with us';
+const getInTouchText = 'You can also get involved with us in meaningful ways—through donations, volunteering, or becoming a member. Every action you take helps us make a greater impact together.';
 
 export default async function Contact() {
     const result = await urqlClient.query<
@@ -182,6 +184,52 @@ export default async function Contact() {
                     allowMultipleExpansion
                 />
             </Section>
+            <Section
+                className={styles.getInTouch}
+                contentClassName={styles.getInTouchContent}
+                childrenContainerClassName={styles.getInTouchChildren}
+            >
+                <div className={styles.getInTouch}>
+                    <Heading
+                        className={styles.heading}
+                    >
+                        Get in touch with us
+                    </Heading>
+                    {getInTouchText}
+                </div>
+                <div className={styles.links}>
+                    <Link
+                        href="/get-involved/volunteer/"
+                        className={styles.link}
+                    >
+                        Volunteer for a cause
+                    </Link>
+                    <Link
+                        href="/donate/"
+                        className={styles.link}
+                    >
+                        Donate
+                    </Link>
+                    <Link
+                        // TODO: Fix this link to download membership form
+                        href="/donate/"
+                        className={styles.link}
+                    >
+                        Become a member
+                    </Link>
+                </div>
+            </Section>
+            <div className={styles.mapContainer}>
+                <iframe
+                    className={styles.map}
+                    title="NRCS location"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28260.721783940204!2d85.25562017910154!3d27.699057300000014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb186127871d01%3A0xffe5ec3bb6afa776!2z4KSo4KWH4KSq4KS-4KSyIOCksOClh-CkoeCkleCljeCksOCkuCDgpLjgpYvgpLjgpL7gpIfgpJ_gpYAg4KSV4KWH4KSo4KWN4KSm4KWN4KSw4KS_4KSvIOCkleCkvuCksOCljeCkr-CkvuCksuCkryAo4KSk4KS-4KS54KS-4KSa4KSyLSDgpKzgpL7gpKvgpLIpIOCkteCkoeCkviDgpKjgpIIu4KWn4KWp!5e0!3m2!1sen!2snp!4v1756272325998!5m2!1sen!2snp"
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                    allowFullScreen
+                />
+
+            </div>
         </Page>
     );
 }
